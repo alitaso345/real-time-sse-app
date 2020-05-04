01977ce94b8bb2e3e5843db76031e6ff103600ca
alitaso345 <alice.maru345@gmail.com>
2020-05-04 10:20:10 +0900

    table

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,81 @@
 import * as React from 'react'
+import { useMemo } from 'react'
+import { useTable, Column } from 'react-table'
 
-type Props = {}
+import { getInitialFlightData, Data } from './DataProvider'
 
-class App extends React.Component<Props> {
-  constructor(props: Props) {
-    super(props)
-  }
-  
-  render() {
-    return (
-      <div>hello</div>
-    )
-  }
+type TableProps = {
+  columns: Column<Data>[]
+  data: Data[]
+}
+
+const Table: React.FC<TableProps> = ({columns, data}) => {
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow
+  } = useTable({ columns, data })
+
+  return (
+    <table {...getTableProps()}>
+      <thead>
+        {headerGroups.map(headerGroup => (
+          <tr {...headerGroup.getHeaderGroupProps()}>
+            {headerGroup.headers.map(column => (
+              <th {...column.getHeaderProps()}>{column.render('Header')}</th>
+            ))}
+          </tr>
+        ))}
+      </thead>
+      <tbody {...getTableBodyProps()}>
+        {rows.map((row, i) => {
+          prepareRow(row)
+          return (
+            <tr {...row.getRowProps()}>
+              {row.cells.map(cell => {
+                return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+              })}
+            </tr>
+          )
+        })}
+      </tbody>
+    </table>
+  )
+}
+
+
+const App: React.FC = () => {
+  const columns: Column<Data>[] = useMemo(
+    () => [
+      {
+        Header: "Origin",
+        accessor: "origin"
+      },
+      {
+        Header: "Flight",
+        accessor: "flight"
+      },
+      {
+        Header: "Arrival",
+        accessor: "arrival"
+      },
+      {
+        Header: "State",
+        accessor: "state"
+      }
+    ],
+    []
+  )
+  const data = useMemo(() => getInitialFlightData(), [])
+
+  return (
+    <Table 
+      columns={columns}
+      data={data}
+    />
+  )
 }
 
 export default App
