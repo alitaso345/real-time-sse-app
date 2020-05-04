e0304604aaa8947ff3814e7c5f298bbec4a8b412
alitaso345 <alice.maru345@gmail.com>
2020-05-04 10:30:57 +0900

    add styled component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,38 @@
 import * as React from 'react'
+import styled from 'styled-components'
 import { useMemo } from 'react'
 import { useTable, Column } from 'react-table'
-
 import { getInitialFlightData, Data } from './DataProvider'
 
 type TableProps = {
   columns: Column<Data>[]
   data: Data[]
 }
+
+const Styles = styled.div`
+  padding: 1rem;
+  table {
+    border-spacing: 0;
+    border: 1px solid black;
+    tr {
+      :last-child {
+        td {
+          border-bottom: 0;
+        }
+      }
+    }
+    th,
+    td {
+      margin: 0;
+      padding: 0.5rem;
+      border-bottom: 1px solid black;
+      border-right: 1px solid black;
+      :last-child {
+        border-right: 0;
+      }
+    }
+  }
+`
 
 const Table: React.FC<TableProps> = ({columns, data}) => {
   const {
@@ -71,10 +96,9 @@ const App: React.FC = () => {
   const data = useMemo(() => getInitialFlightData(), [])
 
   return (
-    <Table 
-      columns={columns}
-      data={data}
-    />
+    <Styles>
+      <Table columns={columns} data={data} />
+    </Styles>
   )
 }
 
